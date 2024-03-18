217886f251f4ee8a26fc08757d13a3b73af007ad
undefinedyobra <brianmelly008@gmail.com>
2024-03-18 13:00:47 +0300

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, GRACE HOPPER ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
